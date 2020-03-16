502c4a8f5ae511435b2aba964a2483860218f3a6
Дмитрий Скурат <dimaskyrat@gmail.com>
2020-03-16 17:22:58 +0000

    load-all-rows (pull request #5)

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,57 @@
 <template>
   <div id="app">
-    <p>{{searchItem}}</p>
+    <p>{{ searchItem }}</p>
     <input placeholder="Enter your search items" v-model="searchItem" />
-    <button @click="sendToConsole">SEARCH</button>
+    <button @click="onSearchButtonClick">SEARCH</button>
+    <div>
+      <!--<table-row v-for="item in fullParsedArray" v-bind:key="item"></table-row>-->
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import {
+  getElemFromResponse,
+  getTable,
+  filterTableRows,
+  hasNextPageElem
+} from "./helpers";
 
-export default {
-  name: "app",
-  data() {
-    return {
-      searchItem: ''
-    };
-  },
-  methods:{
-    sendToConsole(){
-      console.log('SEARCH')
+/*Vue.component("table-row", {
+  props: ["item"],
+  template: "<li>{{item}}</li>"
+});*/
+
+@Component({})
+export default class App extends Vue {
+  searchItem = "";
+  fullParsedArray: HTMLTableRowElement[] = [];
+
+  async onSearchButtonClick() {
+    const pageNum = 0;
+    const url = new URL(location.href);
+    url.searchParams.set("q", this.searchItem);
+    this.loadMore(url, pageNum);
+  }
+  async loadMore(url: URL, pageNum: number) {
+    url.searchParams.set("start", `${50 * pageNum}`);
+    const response = await fetch(url.href);
+    const htmlPage = await response.text();
+    const el = getElemFromResponse(htmlPage);
+    const table = getTable(el);
+
+    if (table === null) {
+      return;
+    }
+    const parsedArray = filterTableRows(table);
+    this.fullParsedArray.splice(0, 0, ...parsedArray);
+    console.log(this.fullParsedArray);
+    if (hasNextPageElem(el)) {
+      this.loadMore(url, pageNum + 1);
     }
   }
-};
+} 
 </script>
 
 <style lang="scss">
---
+++ src/helpers.ts
@@ -0,0 +1,21 @@
+export function getTable(htmlElement: HTMLDivElement) {
+  return htmlElement.querySelector<HTMLTableElement>(".ba-tbl-list__table");
+}
+export function getElemFromResponse(resp: string) {
+  const el = document.createElement("div");
+  el.innerHTML = resp;
+  return el;
+}
+export function filterTableRows(table: HTMLTableElement) {
+  return Array.from(table.rows)
+    .filter(row => {
+      return row.classList.length === 0;
+    })
+}
+export function hasNextPageElem(el: HTMLDivElement) {
+  const nextPageEl = el.querySelector(".page-next");
+  if (!nextPageEl) {
+    return false;
+  }
+  return true;
+}

c0a92b0188eaf6ef45d14091a244e2d892399c4f
DmitrySkyrat <dimaskyrat@gmail.com>
2020-04-18 17:12:30 +0300

    added infinite scroll

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <form @submit.prevent="onFormSubmit">
-      <input placeholder="Enter your search items" v-model="searchItem" />
+      <input placeholder="Enter your search items" v-model="searchText" />
       <input v-model="minPrice" placeholder="Enter min price" type="number" />
       <input v-model="maxPrice" placeholder="Enter max price" type="number" />
       <select v-model="selectedCategoryIndex">
@@ -9,12 +9,20 @@
           v-for="(category, index) in baraholkaCategories"
           :key="category.id"
           :value="index"
-        >{{category.name}}</option>
+          >{{ category.name }}</option
+        >
       </select>
       <input type="submit" value="SEARCH" />
     </form>
     <table class="ba-tbl-list__table">
-      <TableRow v-for="topic in fullParsedArray" v-bind:key="topic.id" v-bind:topic="topic"></TableRow>
+      <TableRow
+        v-for="topic in fullParsedArray"
+        v-bind:key="topic.id"
+        v-bind:topic="topic"
+      ></TableRow>
+      <tr v-show="isLoading">
+        Loading more...
+      </tr>
     </table>
   </div>
 </template>
@@ -31,71 +39,71 @@ import {
   hideNativePagination,
   tableRowsToTopics,
   priceFilter,
-  parseCategories
+  parseCategories,
 } from "./helpers";
 import { BaraholkaTopic, Category } from "./models";
+import { baraholkaDataSource } from "./services/baraholka-data-source"
 
 @Component({
   components: {
-    TableRow
-  }
+    TableRow,
+  },
 })
 export default class App extends Vue {
-  searchItem = "";
+  searchText = "";
   fullParsedArray: BaraholkaTopic[] = [];
   baraholkaCategories: Category[] = [];
   selectedCategoryIndex = 1;
+  pageNum = 0;
   minPrice = 0;
   maxPrice = 0;
+  isLoading = false;
 
   mounted() {
+    hideNativeTable();
+    hideNativePagination();
     const categoriesBlock = document.querySelector(".b-ba-tabs");
     if (!categoriesBlock) {
       return;
     }
-
     this.baraholkaCategories.push(...parseCategories(categoriesBlock));
+    //Infinite topic scroll
+    this.loadMore();
+    document.addEventListener("scroll", (e) => {
+      if (this.isLoading) {
+        return;
+      }
+      const container = document.documentElement;
+      if (
+        container.scrollTop + container.clientHeight + 300 >=
+        container.scrollHeight
+      ) {
+        this.loadMore();
+      }
+    });
   }
 
   async onFormSubmit() {
-    hideNativeTable();
-    hideNativePagination();
-
     this.fullParsedArray.splice(0);
-
-    const pageNum = 0;
-    const url = new URL(location.href);
-    url.pathname = "search.php";
-    url.searchParams.set("q", this.searchItem);
-    url.searchParams.set(
-      "cat",
-      this.baraholkaCategories[this.selectedCategoryIndex].id
-    );
-
-    this.loadMore(url, pageNum);
+    this.pageNum = 0;
+    this.loadMore();
   }
 
-  async loadMore(url: URL, pageNum: number) {
-    url.searchParams.set("start", `${50 * pageNum}`);
-    const response = await fetch(url.href);
-    const htmlPage = await response.text();
-    const el = getElemFromResponse(htmlPage);
-    const table = getTable(el);
+  async loadMore() {
+    this.isLoading = true;
 
-    if (table === null) {
-      return;
-    }
-    const parsedArray = filterTableRows(table);
-    const newParsedArray = tableRowsToTopics(parsedArray);
-    const filteredTopics = newParsedArray.filter(topic =>
+    const newParsedArray = await baraholkaDataSource.getTopics({
+      searchText: this.searchText,
+      category: this.baraholkaCategories[this.selectedCategoryIndex],
+      pageNum: this.pageNum,
+    });
+
+    const filteredTopics = newParsedArray.topics.filter((topic) =>
       priceFilter(topic.price, this.minPrice, this.maxPrice)
     );
-
     this.fullParsedArray.push(...filteredTopics);
-
-    if (hasNextPageElem(el)) {
-      this.loadMore(url, pageNum + 1);
-    }
+    this.isLoading = false;
+    this.pageNum++;
   }
 }
 </script>

bad288e005648641bd1ca31d7c9a4a2b4c307f22
DmitrySkyrat <dimaskyrat@gmail.com>
2020-04-19 11:18:33 +0300

    added city filter

---
+++ src/components/SearchItems.vue
@@ -0,0 +1,78 @@
+<template>
+  <form @submit.prevent="onFormSubmit" class="form">
+    <input placeholder="ВВЕДИТЕ НАЗВАНИЕ ТОВАРА" v-model="searchText" class="search-style" />
+    <select v-model="selectedCategoryIndex" class="category-style">
+      <option
+        v-for="(category, index) in baraholkaCategories"
+        :key="category.id"
+        :value="index"
+        >{{ category.name }}</option
+      >
+    </select>
+    <v-select
+      :options="baraholkaCities"
+      label="name"
+      placeholder="ВЫБЕРИТЕ ГОРОД"
+      class="city-style"
+      v-model="selectedCities"
+      multiple
+    ></v-select>
+    <input v-model="minPrice" placeholder="МИНИМАЛЬНАЯ ЦЕНА" type="number" class="min-style" />
+    <input v-model="maxPrice" placeholder="МАКСИМАЛЬНАЯ ЦЕНА" type="number" class="max-style" />
+    <button class="submit-style">ПОИСК</button>
+  </form>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Mounted } from "vue-class-decorator";
+import vSelect from "vue-select";
+import { parseCategories } from "../helpers";
+import { BaraholkaTopic, Category, City } from "../models";
+import { baraholkaDataSource } from "../services/baraholka-data-source";
+
+Vue.component("v-select", vSelect);
+
+@Component({})
+export default class Params extends Vue {
+  searchText = "";
+  minPrice = 0;
+  maxPrice = 0;
+  selectedCategoryIndex = 0;
+  selectedCities: City[] = [];
+  baraholkaCities: City[] = [];
+  baraholkaRegionsID: string[] = [];
+  baraholkaCategories: Category[] = [];
+
+  async created() {
+    const regions = await baraholkaDataSource.getRegions();
+    this.baraholkaRegionsID.push(...regions.map((region) => region.id));
+
+    const cities = await Promise.all(
+      this.baraholkaRegionsID.map((id) => baraholkaDataSource.getCities(id))
+    );
+    cities.forEach((IdArray) => this.baraholkaCities.push(...IdArray));
+    this.baraholkaCities.sort((a, b) => (a.name > b.name ? 1 : -1));
+  }
+
+  @Mounted()
+  getCategories() {
+    const categoriesBlock = document.querySelector(".b-ba-tabs");
+    if (!categoriesBlock) {
+      return;
+    }
+    this.baraholkaCategories.push(...parseCategories(categoriesBlock));
+    this.onFormSubmit();
+  }
+
+  onFormSubmit() {
+    const searchData = {
+      searchText: this.searchText,
+      minPrice: this.minPrice,
+      maxPrice: this.maxPrice,
+      selectedCities: this.selectedCities,
+      selectedCategory: this.baraholkaCategories[this.selectedCategoryIndex],
+    };
+    this.$emit("change", searchData);
+  }
+}
+</script>
--- src/services/baraholka-data-source.ts
+++ src/services/baraholka-data-source.ts
@@ -1,37 +1,77 @@
 import {
-    getElemFromResponse,
-    getTable,
-    filterTableRows,
-    hasNextPageElem,
-    tableRowsToTopics,
-  } from "../helpers";
+  getElemFromResponse,
+  getTable,
+  filterTableRows,
+  hasNextPageElem,
+  tableRowsToTopics
+} from "../helpers";
 
-  import { BaraholkaTopic, BaraholkaSearchParams } from "../models";
+import { BaraholkaTopic, BaraholkaSearchParams, Region, City } from "../models";
 
-  class BaraholkaDataSource {
-    async getTopics(
-      params: BaraholkaSearchParams
-    ): Promise<{ topics: BaraholkaTopic[]; hasNextPage: boolean }> {
-      const url = new URL(location.href);
-      url.pathname = "search.php";
-      url.searchParams.set("q", params.searchText);
-      url.searchParams.set("cat", params.category.id);
-      url.searchParams.set("start", `${50 * params.pageNum}`);
-      const response = await fetch(url.href);
-      const htmlPage = await response.text();
-      const el = getElemFromResponse(htmlPage);
-      const table = getTable(el);
-      const nextPagePresenceValue = hasNextPageElem(el);
-      if (table === null) {
-        return { topics: [], hasNextPage: nextPagePresenceValue };
-      }
-      const parsedArray = filterTableRows(table);
-      const newParsedArray = tableRowsToTopics(parsedArray);
-      return {
-        topics: newParsedArray,
-        hasNextPage: nextPagePresenceValue,
-      };
+class BaraholkaDataSource {
+  async getTopics(
+    params: BaraholkaSearchParams
+  ): Promise<{ topics: BaraholkaTopic[]; hasNextPage: boolean }> {
+    const url = new URL(location.href);
+    url.pathname = "search.php";
+    url.searchParams.set("q", params.searchText);
+    url.searchParams.set("cat", params.category.id);
+    url.searchParams.set("start", `${50 * params.pageNum}`);
+    const response = await fetch(url.href);
+    const htmlPage = await response.text();
+    const el = getElemFromResponse(htmlPage);
+    const table = getTable(el);
+    const nextPagePresenceValue = hasNextPageElem(el);
+    if (table === null) {
+      return { topics: [], hasNextPage: nextPagePresenceValue };
     }
+    const parsedArray = filterTableRows(table);
+    const newParsedArray = tableRowsToTopics(parsedArray);
+    return {
+      topics: newParsedArray,
+      hasNextPage: nextPagePresenceValue
+    };
   }
-  
-  export const baraholkaDataSource = new BaraholkaDataSource();
+  async  getCities(idNumber: string): Promise<City[]> {
+    const formData = new URLSearchParams();
+    formData.set("regionId", idNumber);
+    const url = new URL(location.href);
+    url.pathname = "getCities.php";
+    const response = await fetch(url.href, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        accept: "application/json, text/javascript, */*; q=0.01",
+        "x-requested-with": "XMLHttpRequest",
+      },
+      body: formData,
+    });
+    const respBody = await response.json();
+    return respBody.cities.map((citiesElem: any) => {
+      const city: City = {
+        name: citiesElem.name,
+        id: citiesElem.id,
+      };
+      return city;
+    });
+  }
+   async  getRegions(): Promise<Region[]> {
+    const response = await fetch(
+      "https://baraholka.onliner.by/fleamarketposting.php"
+    );
+    const htmlPage = await response.text();
+    const el = getElemFromResponse(htmlPage);
+    const regionElements: NodeListOf<HTMLOptionElement> = el.querySelectorAll(
+      ".js-posting-region option"
+    );
+    return Array.from(regionElements).map((regionElem) => {
+      const region: Region = {
+        id: regionElem.value,
+        name: regionElem.innerText,
+      };
+      return region;
+    });
+  }
+}
+
+export const baraholkaDataSource = new BaraholkaDataSource();
